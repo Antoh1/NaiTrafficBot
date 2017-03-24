dc914a73df54317aeb3ba02fbdc655facb207e3e
Antoh1 <antotiro@gmail.com>
2017-03-24 04:38:27 +0300

    Updated PAT and VT environment tokens

--- traffic-bot.py
+++ traffic-bot.py
@@ -8,17 +8,13 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-PAGE_ACCESS_TOKEN = 'EAASRv3v3qVoBAMbiIuu0N9GWekRh7g7kj4C99vSsDgpBybAEOTaZBThnwvXYDTVaQl56qET6R3tujYebOAgZA8wG3z2CyLF1NGYZC4TABTZAmiBuc6f3Utby5ZApN5J8brm0ivR44GzhXH0raNc6BZBckO9GJYNVLxqAxFOivVLAZDZD'
-
-VERIFY_TOKEN = 'my_token_for_verification'
-
 
 @app.route('/', methods=['GET'])
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == VERIFY_TOKEN:
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
@@ -56,7 +52,7 @@ def send_message(recipient_id, message_text):
 
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
-    params = {"access_token": PAGE_ACCESS_TOKEN}
+    params = {"access_token": os.environ["PAGE_ACCESS_TOKEN"]}
     
     headers = {"Content-Type": "application/json"}
     
